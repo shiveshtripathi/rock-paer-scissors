0b4a4969c97f6cd41dc2b0a43ff8ecb7a6ac3cda
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2024-07-22 13:26:32 +0530

    Complete the playGame function to play 5 rounds and decide the winner

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 
 const log = console.log //Ease in outputting //
 
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
   const gameOptions = [`Rock`, `Paper`, `Scissors`]
   const randomNumber = Math.floor(Math.random() * 3) //Random number between 0 & 2//
-  log(
-    `The Random Number is ${randomNumber} and the Computer choice is ${gameOptions[randomNumber]}`
-  )
+  // log(
+  //   `The Random Number is ${randomNumber} and the Computer choice is ${gameOptions[randomNumber]}`
+  // )
   return gameOptions[randomNumber]
 }
 
@@ -18,9 +21,54 @@ function getHumanChoice() {
     `Please enter your choice(Rock, Paper or Scissors)`,
     `${gameOptions[randomNumber]}`
   )
-  log(
-    `The Random Number is ${randomNumber} and the Human choice is ${humanChoice}`
-  )
+  // log(
+  //   `The Random Number is ${randomNumber} and the Human choice is ${humanChoice}`
+  // )
+  return humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase()
 }
-const computerChoice = getComputerChoice()
-const humanChoice = getHumanChoice()
+
+function playGame() {
+  const computerSelection = getComputerChoice()
+  const humanSelection = getHumanChoice()
+
+  function playRound(computerSelection, humanSelection) {
+    const humanChoice = humanSelection.toLowerCase()
+    const computerChoice = computerSelection.toLowerCase()
+    if (humanChoice === computerChoice) {
+      console.log(
+        `It's a Draw! ${humanSelection} befriends ${computerSelection}`
+      )
+    } else if (computerChoice === `rock` && humanChoice === `paper`) {
+      console.log(`You Win! ${humanSelection} beats ${computerSelection}`)
+      humanScore += 1
+    } else if (computerChoice === `scissors` && humanChoice === `rock`) {
+      console.log(`You Win! ${humanSelection} beats ${computerSelection}`)
+      humanScore += 1
+    } else if (computerChoice === `paper` && humanChoice === `scissors`) {
+      console.log(`You Win! ${humanSelection} beats ${computerSelection}`)
+      humanScore += 1
+    } else {
+      console.log(`You Lose! ${computerSelection} beats ${humanSelection}`)
+      computerScore += 1
+    }
+  }
+
+  playRound(computerSelection, humanSelection)
+}
+
+for (let counter = 1; counter < 6; counter++) {
+  playGame()
+}
+
+if (humanScore > computerScore) {
+  console.log(
+    `You Win! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
+  )
+} else if (computerScore > humanScore) {
+  console.log(
+    `You Lose! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
+  )
+} else
+  console.log(
+    `It's a Draw! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
+  )
