8f043e712bba7a8d8d5f9ecb4d9980ae3e3c7fc0
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2024-10-22 13:27:03 +0530

    Add support for input from button and likewise announce the result

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 'use strict'
 
 const log = console.log //Ease in outputting //
+const userInput = document.querySelector('.userInput')
 
 let humanScore = 0
 let computerScore = 0
+
+userInput.addEventListener('click', function (event) {
+  // Check if the clicked element is a button
+  if (event.target.tagName === 'BUTTON') {
+    // Check which button was clicked using its class name
+    if (event.target.classList.contains('rock-btn')) {
+      playGame('Rock')
+    } else if (event.target.classList.contains('paper-btn')) {
+      playGame('Paper')
+    } else if (event.target.classList.contains('scissors-btn')) {
+      playGame('Scissors')
+    }
+  }
+})
 
 function getComputerChoice() {
   const gameOptions = [`Rock`, `Paper`, `Scissors`]
@@ -21,9 +36,9 @@ function getComputerChoice() {
 //   return humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase()
 // }
 
-function playGame() {
+function playGame(humanChoice) {
   const computerSelection = getComputerChoice()
-  const humanSelection = getHumanChoice()
+  // const humanSelection = getHumanChoice()
 
   function playRound(computerSelection, humanSelection) {
     const humanChoice = humanSelection.toLowerCase()
@@ -47,22 +62,24 @@ function playGame() {
     }
   }
 
-  playRound(computerSelection, humanSelection)
+  playRound(computerSelection, humanChoice)
+  printResult()
 }
 
-for (let counter = 1; counter < 6; counter++) {
-  playGame()
+// for (let counter = 1; counter < 6; counter++) {
+//   playGame()
+// }
+function printResult() {
+  if (humanScore > computerScore) {
+    console.log(
+      `You Win! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
+    )
+  } else if (computerScore > humanScore) {
+    console.log(
+      `You Lose! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
+    )
+  } else
+    console.log(
+      `It's a Draw! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
+    )
 }
-
-if (humanScore > computerScore) {
-  console.log(
-    `You Win! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
-  )
-} else if (computerScore > humanScore) {
-  console.log(
-    `You Lose! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
-  )
-} else
-  console.log(
-    `It's a Draw! You won ${humanScore} rounds while Computer won ${computerScore} rounds`
-  )
